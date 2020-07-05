ab0b40f3f41df32c1fd0c4894af8bbfbf8376d87
anthonygedeon <anthony.gedeonn@gmail.com>
2020-07-05 11:20:07 -0500

    Making URL components dynamic by using fake data

---
+++ src/scripts/data.js
@@ -0,0 +1,10 @@
+const data = [
+    {
+        globalHash: '232304',
+        hash: 'k42dZs',
+        longURL: 'https://google.com/',
+        url: 'https://shrt.com/k42dZs',
+    }
+];
+
+export default data;
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,7 @@
 import '../scss/main.scss';
 
+import data from './data';
+
 const menu = document.querySelector('.menu');
 const menuModal = document.querySelector('.menu-modal');
 
@@ -16,3 +18,53 @@ copyBtns.forEach((copyBtn) => {
         event.target.style.pointerEvents = 'none';
     });
 });
+
+const Model = (() => {
+    const newData = [...data];
+
+    return {
+        newData,
+    };
+})();
+
+const View = (() => {
+    const urlContainer = document.querySelector('.url');
+
+    const copyButtons = document.querySelectorAll('.js-copy-btn');
+    const shortenURLText = document.querySelectorAll('.shorten-copy__shorten');
+
+    const copyToClipBoard = () => {
+
+    };
+
+    const render = (urls) => {
+        urls.forEach((url) => {
+            const component = `
+                <div class="shorten-copy" >
+                    <p class="shorten-copy__original">
+                        ${url.longURL}
+                    </p>
+                    <hr class="shorten-copy__line">
+                    <p class="shorten-copy__shorten">${url.url}</p>
+                    <button
+                        class="button button--primary button--full-width js-copy-btn"
+                    >
+                        Copy
+                    </button>
+                </div>
+            `;
+
+            return urlContainer.insertAdjacentHTML('beforeend', component);
+        });
+    };
+
+    return {
+        render,
+    };
+})();
+
+const Controller = ((model, view) => {
+    const data = model.newData;
+
+    view.render(data);
+})(Model, View);
