b8e69d0cdc471fc4ac7d32ee8a75e07668320cf5
Anthony Gedeon <anthony.gedeonn@gmail.com>
2021-06-28 20:53:18 -0500

    create: cta component

---
+++ src/components/Cta.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="cta">
+    <h3 class="cta__heading">Boost your links today</h3>
+    <button class="button button--md">Get Started</button>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.cta {
+  padding: 5rem 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: url("../assets/bg-boost-mobile.svg") no-repeat top right
+    var(--main-bg-color);
+
+  &__heading {
+    text-align: center;
+    font-weight: var(--weight-bold);
+    color: var(--white-color);
+    font-size: 1.5rem;
+    margin-top: 0;
+  }
+}
+</style>
